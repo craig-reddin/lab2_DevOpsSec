72c00ab71faf94b4e0538fc57949e0a6520e41cc
Craig Reddin <craig-reddin@hotmail.com>
2025-01-30 13:20:07 +0000

    third commit

--- script.sh
+++ script.sh
@@ -1,27 +1,39 @@
 #!/bin/bash
 
+rm  -rf fol_1
+rm -rf fol_2
+
 mkdir fol_1
 
 mkdir fol_2
 
 cd fol_1
 
-rm 1_1.txt
+touch 1_1.txt
 
-rm 1_2.txt
+touch 1_2.txt
 
-rm 1_3.txt
+touch 1_3.txt
 
-cd.
+
+
+cd -
 
 cd fol_2
 
-rm 2_1.txt
+touch 2_1.txt
 
-rm 2_2.txt
+touch 2_2.txt
 
-rm 2_3.txt
+touch 2_3.txt
 
+cd -
+
+chmod 600 fol_1/1_1.txt fol_1/1_3.txt fol_2/2_1.txt fol_2/2_3.txt
+
+chmod 777 fol_1/1_2.txt fol_2/2_2.txt
+
+echo "Finished"
 
 
 
